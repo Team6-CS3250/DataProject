f1134e39548de8c365a4beec769379524816e645
bleuflame47 <78895962+bleuflame47@users.noreply.github.com>
2022-02-28 21:32:35 -0700

    Created seperate CRUD class to simplify use in GUI

--- DataProject/DatabaseWarehouse.py
+++ DataProject/DatabaseWarehouse.py
@@ -22,8 +22,13 @@ class databaseWarehouse():
     def __init__(self):
         self.base = Tk()
         self.frm = ttk.Frame(self.base, padding=10)
-        ttk.Label(self.base, text="Hello World!").grid(column=0, row=0)
-        ttk.Button(self.base, text="Quit", command=self.base.destroy).grid(column=1, row=0)
+        ttk.Label(self.base, text="What would you like to do?").grid(column=0, row=0)
+    
+        self.CRUD_frame = ttk.Frame(self.base, padding = 5)
+
+        
+        ttk.Button(self.base, text="Select", command=self.base.destroy).grid(column=1, row=0)
+        ttk.Button(self.base, text="Quit", command=self.base.destroy).grid(column=2, row=0)
 
     def run(self):
         self.base.mainloop()
