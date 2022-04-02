a60382887b077ba9a8b9e32484d33f6aa3e1e407
bleuflame47 <78895962+bleuflame47@users.noreply.github.com>
2022-04-02 09:59:26 -0600

    Cleaned up CRUD methods, started connecting to frontend, deleted duplicate

--- DataProject/Operations.py
+++ DataProject/Operations.py
@@ -1,7 +1,12 @@
-from sqlite3 import Error
 import sqlite3
-import pandas as pd
+from sqlite3 import Error
+from this import d
 
+from frontend_GUI import filename
+from frontend_GUI import user_addition
+from frontend_GUI import user_update
+from frontend_GUI import user_deletion
+import pandas as pd
 
 
 class Ops():
@@ -17,7 +22,8 @@ class Ops():
         #  In addition to accessing the customer_orders.db, is it not important to 
         # also include the inventory.db in all these operations
         
-        db_file = db_file
+        # connects to frontend to retrieve user requested file
+        db_file = filename
         con = None
         try: #Trying to connect to the given DB file path.
             self.database = sqlite3.connect(db_file)
@@ -32,19 +38,16 @@ class Ops():
                 con.close()
 
 
-
     def formatTable(self):
         """Fixer method that is to format the table, use only if the table is not in the DB. """
         
-        csv_file_name = 'customer_orders_team6.csv'
-        #  I'm getting an FileNotFoundError on the line below.  Any suggestions as to how to fix this?
+        csv_file_name = 'customer_orders_team6.csv' # will be 'filename'
         data = pd.read_csv(csv_file_name)
         df = pd.DataFrame(data)
         cur = self.database.cursor()
         cur.execute("CREATE TABLE IF NOT EXISTS customer_orders(date TEXT, cust_email TEXT, cust_location INTEGER, product_id TEXT, product_quantity INTEGER)")
         data.to_sql('customer_orders', self.database, if_exists='replace', index=False)
         self.database.commit()
-
 
 
     def viewTable(self):
@@ -56,15 +59,21 @@ class Ops():
         df = pd.read_sql_query("SELECT * from customer_orders", con)
         return(df)
 
+
     def saveChanges(self):
         """ When called it saves any changes that were made to the DB."""
 
         self.database.commit()
 
+    #  Do we want to just "assertTrue" whether the ID exists in the file? 
+    # or do we want to return the entire entry? (Though this operation is completed
+    # in the find function)
+    # or view the whole table?
     def view(self):
         """ This function is used to find an ID from the DB. """
         
         return
+
 
     def find(self, key, entry):
         """ This function is used to return the ID given to it. find('key','entry')
@@ -90,48 +99,60 @@ class Ops():
         repo = pd.DataFrame(cur.fetchall(), columns=['date', 'cust_email', 'cust_location', 'product_id', 'product_quantity'])
         print(repo)
         
-    def add(self):
+    def add(self, entry):
         """ CRUD function, that inserts a series of variables into a new entry in the DB. """
         
-        cur = self.cur
-        con = self.database
-        
-        query = ('INSERT INTO INVENTORY(PRODUCT_ID, QUANTITY, WHOLESALE_COST, SALE_PRICE, SUPPLIER_ID' 
-                    'VALUES (:PRODUCT_ID, :QUANTITY, :WHOLESALE_COST, :SALE_PRICE, :SUPPLIER_ID);')
-        new_entry = {}
-        
-        con.execute(query, new_entry)
-        con.commit()
-        
-        con.close()
+        query = ('insert into customer_orders (date, cust_email, cust_location, product_id, product_quantity)' 'VALUES (:date, :cust_email, :cust_location, :product_id, :product_quantity)')
+        new_entry = user_addition
 
-    def edit(self):
+        try: 
+            cur = self.cur
+            con = self.database
+            con.execute(query, new_entry)
+            con.commit()
+        
+        except Error as e:
+            print(e)
+            
+        finally:
+            if con:
+                con.close()
+
+    def edit(self, entry):
         """ This function edits the ID that is recived."""
+        query = 'UPDATE customer_orders set date=?, cust_email=?, cust_location=?, product_id=?, product_quantity=? where date=?, cust_email=?, cust_location=?, product_id=?, product_quantity=?'
+        updated_entry= user_update
+        
+        try:
+            cur = self.cur
+            con = self.database
+            con.execute(query, updated_entry)
+            con.commit()
+        
+        except Error as e:
+            print(e)
 
-        cur = self.cur
-        con = self.database
+        finally:
+            if con:
+                con.close()
 
-        con.execute('UPDATE INVENTORY set ROLL = 005 where ID = 1')
-        con.commit()
+    def delete(self, entry):
+        """ This function deletes the ID currently being recieved. """
+        query = 'DELETE from customer_orders where date=?, cust_email=?, cust_location=?, product_id=?, product_quantity=?'
+        del_selection = user_deletion
 
-        cur = con.find("SELECT * from INVENTORY")
-        print(cur.fetchall())
+        try:
+            cur = self.cur
+            con = self.database
+            con.execute(query, del_selection)
+            con.commit()
 
-        con.close()
-
-    def delete(self):
-        """ This function deletes the ID currently being recived. """
-
-        cur = self.cur
-        con =self.database
-
-        con.execute("DELETE from INVENTORY where ID = 1;")
-        con.commit()
-
-        cur = con.execute("SELECR * from INVENTORY")
-        print(cur.fetchall)
-
-        con.close()
+        except Error as e:
+            print(e)
+        
+        finally:
+            if con:
+                con.close()
 
     def exit(self):
         """ This function will close out of the DB. """
