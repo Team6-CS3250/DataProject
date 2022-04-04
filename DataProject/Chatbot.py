013c76244a18e5c59298e5b1ae8f595e82022f50
Cisco Yslas <cyslas94@hotmail.com>
2022-04-04 13:36:48 -0600

    Scaffolding for chatbot, having trouble with dependencies for wordnet

---
+++ DataProject/Chatbot.py
@@ -0,0 +1,30 @@
+import nltk
+import re
+nltk.download('wordnet')
+from nltk.corpus import wordnet
+#Chatbot scaffolding and psuedocode
+#re is a package for regular expressions
+#Helps with a dictionary of synonyms for keywords
+
+#Dependencies that need to be imported
+
+class Chatbot():
+    #List of keywords to respond to
+    #Hello, order, stock, shipping
+    list_words = ['Hello', 'Order', 'Stock', 'Shipping']
+
+    list_syn = {}
+
+    for word in list_words:
+        synonyms = []
+        
+        for syn in wordnet.synsets(word):
+
+            for lem in syn.lemmas():
+                #Remove any special characters from synonym strings
+                lem_name = re.sub('[^a-zA-Z0-9 \n\.]', ' ', lem.name())
+                synonyms.append(lem_name)
+
+        list_syn[word] = set(synonyms)
+    
+    print(list_syn)
